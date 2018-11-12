ada587b6603a10f2061c3e3d7ba80fc41425b0f6
bennett39 <bennettgarner@gmail.com>
2018-11-12 08:19:53 -0500

    Add views

--- django/garnereng/sitereport/urls.py
+++ django/garnereng/sitereport/urls.py
@@ -5,4 +5,6 @@ from . import views
 # URL paths:
 urlpatterns = [
     path('', views.index, name='index'),
+    path('client/', views.client, name='client'),
+    path('project/', views.project, name='project'),
 ]
--- django/garnereng/sitereport/views.py
+++ django/garnereng/sitereport/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Project
+
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello world - site report index.")
+    context = {
+        'projects': Project.objects.all()
+    }
+    return render(request, "sitereport/index.html", context)
+
+def client(request):
+    return HttpResponse("Client index")
+
+def project(request):
+    return HttpResponse("Project index")
